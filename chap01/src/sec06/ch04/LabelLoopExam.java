cd974faee55df82ca677bbf494900040cd6a452f
je-pa <jieun0502@gmail.com>
2021-03-31 16:44:43 +0900

    2021.03.31_label

---
+++ chap01/src/sec06/ch04/LabelLoopExam.java
@@ -0,0 +1,15 @@
+package sec06.ch04;
+
+public class LabelLoopExam {
+	public static void main(String[] args) {
+		OUTER: //label
+		for(int i=0; i<3 ; i++) {
+			for(int z=100; z<105; z++) {
+				System.out.printf("%d - %d \n ",i,z);
+				if(z==102) {
+					break OUTER;
+				}
+			}
+		}
+	}
+}
