440dd6ef6d9d5dc5acdbb7a4e76dd4d5340d5fc1
je-pa <jieun0502@gmail.com>
2021-04-05 16:52:06 +0900

    2021.04.05_자판기 시작

---
+++ chap01/src/sec06/ch05/MethodMission10.java
@@ -0,0 +1,28 @@
+package sec06.ch05;
+
+import java.util.Scanner;
+
+public class MethodMission10 {
+	public static void printMenu(String[] menuNmArr, int[] menuPriceArr) {
+		System.out.println("<메뉴>");
+		for(int i=0;i<menuNmArr.length;i++) {
+			System.out.printf("%d. %s (%,d원)\n",i+1,menuNmArr[i],menuPriceArr[i]);
+		}
+	}
+	
+	public static void printMenu2(String[] menuNmArr, int[] menuPriceArr) {
+		String str= "<메뉴>\n";
+		for(int i=0;i<menuNmArr.length;i++) {
+			str += String.format("%d. %s (%,d원)\n",i+1,menuNmArr[i],menuPriceArr[i]);
+		}
+		System.out.println(str);
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan =new Scanner(System.in);
+		String[] menuNmArr = {"콜라","사이다","환타","스프라이트","망고주스","포도주스"};
+		int[] menuPriceArr = {1000, 1200, 1300, 1500, 2000, 3000};
+		
+		printMenu2(menuNmArr, menuPriceArr);
+	}
+}
