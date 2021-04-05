39ac19488f5ce9021fed5e44ecd94151fc1665b1
je-pa <jieun0502@gmail.com>
2021-04-05 16:52:29 +0900

    2021.04.05_큰값찾기

---
+++ chap01/src/sec06/ch05/MethodMission.java
@@ -0,0 +1,29 @@
+package sec06.ch05;
+
+public class MethodMission {
+	public static void main(String[] args) {
+		whoIsBig(10,30);
+		whoIsBig(10,9);
+		whoIsBig(11,25);
+		
+		int big = getBigNum(10,30);
+		System.out.println("big : "+big);
+		
+		big = getBigNum(10,9);
+		System.out.println("big : "+big);
+	}
+	public static void whoIsBig(int n1, int n2) {
+		int big=n1;
+		if(big<n2) {
+			big=n2;
+		}
+		
+		System.out.printf("%d이 가장 큽니다.\n",big);
+	}
+	public static int getBigNum(int n1, int n2) {
+		if(n1<n2) {
+			return n2;
+		}
+		return n1;
+	}
+}
