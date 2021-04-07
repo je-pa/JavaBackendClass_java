5364ee805a9ae0d42cae2be694efa789985c7543
je-pa <jieun0502@gmail.com>
2021-04-07 17:02:41 +0900

    2021.04.04_캡슐화 name, age

---
+++ chap01/src/sec06/ch06/CapsuleExam2.java
@@ -0,0 +1,10 @@
+package sec06.ch06;
+
+public class CapsuleExam2 {
+	public static void main(String[] args) {
+		Human h1= new Human();
+		Human h2= new Human("평강공주");
+		Human h3= new Human(30);
+		Human h4= new Human("이순신",20);
+	}
+}
