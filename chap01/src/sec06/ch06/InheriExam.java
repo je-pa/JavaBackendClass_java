9ba9778ab96a3745a629d900e601d1429774fb6a
je-pa <jieun0502@gmail.com>
2021-04-08 16:55:46 +0900

    2021.04.08_상속

---
+++ chap01/src/sec06/ch06/InheriExam.java
@@ -0,0 +1,57 @@
+package sec06.ch06;
+
+public class InheriExam {
+	public static void main(String[] args) {
+		Cat cat = new Cat();
+		cat.name = "고양이";
+		cat.age=4;
+		cat.howling();
+		cat.lick();
+	}
+}
+
+class Animal{
+	String name;
+	int age;
+	
+	
+	
+	public Animal(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+
+
+	void howling() {
+		System.out.println("운다");
+	}
+}
+
+class Cat extends Animal{// cat 객체을 만들려 할때! animal을 만들려하고 
+					//animal을 하려 하니까 object 만들려하고
+					//생성자는 호출은 cat->animal->object
+					///	 반환은 object->animal->cat (stack영역에 쌓이기 때문)
+					//메소드는 cat에서 먼저 찾아보고 없으면 animal로 찾으러감/
+	Cat(){
+		super("'",0);
+	}
+	
+	void lick() {
+		System.out.printf("%s가 햝다\n",name);
+	}
+	
+	@Override
+	void howling() {
+		System.out.println("야옹");
+	}
+}
+/*
+class KoShort extends Cat{
+	
+	
+	void pee() {
+		System.out.printf("%s가 소변을 본다 \n",name);
+	}
+}*/
